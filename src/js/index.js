3e7e005ccacc1bb3dd6676b8a3a3fd93538eb253
NashibJr <nashibjr@gmail.com>
2025-03-06 11:51:16 +0300

    Feat: Finishing the todo app

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,6 @@ const deleteButtons = document.querySelectorAll(".todo-item button");
 
 // localStorage.setItem("tasks", JSON.stringify([]));
 
-for (let button of deleteButtons) {
-  button.style.display = "none";
-}
-
-for (let index = 0; index < tasks.length; index++) {
-  tasks[index].addEventListener("mouseover", () => {
-    for (let child of tasks[index].children) {
-      child.style.display = "block";
-    }
-  });
-
-  tasks[index].addEventListener("mouseleave", () => {
-    for (let child in tasks[index].children) {
-      tasks[index].children[tasks[index].children.length - 1].style.display =
-        "none";
-    }
-  });
-}
-
 const taskItems = JSON.parse(localStorage.getItem("tasks"));
 const form = document.querySelector("form");
 
@@ -36,4 +17,39 @@ form.addEventListener("submit", (event) => {
 
   alert(`${input.value} task successfully added`);
   input.value = "";
+  window.location.reload();
 });
+
+document.getElementById("clear-btn").addEventListener("click", () => {
+  taskItems.splice(0, taskItems.length);
+  localStorage.setItem("tasks", JSON.stringify([]));
+  window.location.reload();
+});
+
+(() => {
+  const tasksDiv = document.querySelector(".tasks");
+  document.getElementById("number-of-tasks").innerHTML = taskItems.length;
+
+  taskItems.forEach((taskItem) => {
+    const item = document.createElement("div");
+    item.className = "todo-item";
+    const taskText = document.createElement("p");
+    taskText.innerText = taskItem.text;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.style.display = "none";
+    deleteBtn.onclick = () => {
+      const itemToBeDeletedIndex = taskItems.findIndex(
+        (item) => item.id === taskItem.id
+      );
+      taskItems.splice(itemToBeDeletedIndex, 1);
+      localStorage.setItem("tasks", JSON.stringify(taskItems));
+      window.location.reload();
+    };
+    item.onmouseover = () => (deleteBtn.style.display = "block");
+    item.onmouseleave = () => (deleteBtn.style.display = "none");
+    item.appendChild(taskText);
+    item.appendChild(deleteBtn);
+    tasksDiv.appendChild(item);
+  });
+})();
